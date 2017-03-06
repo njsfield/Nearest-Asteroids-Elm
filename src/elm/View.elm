73a3c3e7039c77933fa471104b671431bba07608
Nick Field <heyimnick@live.com>
2017-03-06 12:02:41 +0000

    add viewNavigation function in view.elm
    related #6

--- src/elm/View.elm
+++ src/elm/View.elm
@@ -9,18 +9,35 @@ import Html.Attributes exposing (..)
 view : Model -> Html Msg
 view model =
     div [ class "tc mt5" ]
-        [ [ div [ class "flex" ]
-                [ button [ onClick PreviousSetting ] [ text "<" ]
-                , h2 [ class "sans-serif" ]
-                    [ text model.settingtext ]
-                    button
-                    [ onClick NextSetting ]
-                    [ text ">" ]
-                ]
-          ]
+        [ viewNavigation model.setting
         , p [ class "sans-serif" ] [ text model.asteroidsErr ]
         , ul [] [ List.map viewListItem model.asteroids ]
         , h2 [ class "sans-serif" ] [ text model.date ]
+        ]
+
+
+viewNavigation : Setting -> Html Msg
+viewNavigation setting =
+    let
+        settingtext =
+            case setting of
+                Name ->
+                    "Name"
+
+                MinSize ->
+                    "Min Size"
+
+                Speed ->
+                    "Speed"
+
+                MissDistance ->
+                    "Miss Distance"
+    in
+        [ div [ class "flex" ]
+            [ button [ onClick PreviousSetting ] [ text "<" ]
+            , h2 [ class "sans-serif" ] [ text settingtext ]
+            , button [ onClick NextSetting ] [ text ">" ]
+            ]
         ]
 
 
