c0e935ff37ac04a4849aaabbeb027bbb323fe08e
Nick Field <heyimnick@live.com>
2017-03-12 09:06:39 +0000

    chance default y grid value to 200

--- src/elm/Model.elm
+++ src/elm/Model.elm
@@ -46,7 +46,7 @@ type alias Height =
 initialModel : Model
 initialModel =
     { date = "2017-03-02"
-    , grid = ( 500, 100 )
+    , grid = ( 500, 200 )
     , setting = Name
     , asteroids =
         [ { name = "(2017 BU6)"
