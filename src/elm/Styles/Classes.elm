69b10550c740bdd9f2e96a1eb08488f0284558b2
Nick Field <heyimnick@live.com>
2017-03-19 14:00:54 +0000

    fix font size of svg text

--- src/elm/Styles/Classes.elm
+++ src/elm/Styles/Classes.elm
@@ -3,10 +3,10 @@ module Styles.Classes exposing (tachs)
 
 tachs =
     { main = "tc w-100 vh-100 flex flex-column items-center justify-center bg-dark-blue"
-    , svgcontainer = "tc dib w-80 pa-10 mw8"
-    , svg = "svg-p overflow-visible"
+    , svgcontainer = "tc dib w-80 pa-10 mw7"
+    , svg = "ma-10 overflow-visible"
     , date = "sans-serif washed-blue tracked-mega fw3"
-    , svgtext = "f60 sans-serif"
+    , svgtext = "f5 sans-serif"
     , svgtextcol = "rgb(130,130,130)"
     , nav = "flex justify-center"
     , navbutton = "bn pointer bg-transparent outline-0 washed-blue pa4 pb0 pt0"
