e114d06abaea74c28709aace40590a6cd9b9622d
Nick Field <heyimnick@live.com>
2017-04-07 10:42:31 +0100

    add additional asteroids to initialModel

--- src/elm/Model.elm
+++ src/elm/Model.elm
@@ -52,6 +52,26 @@ initialModel =
           , speed = 0.234
           , missdistance = 0.342
           }
+        , { name = "(2013 D6)"
+          , minsize = 0.0534324
+          , speed = 0.134
+          , missdistance = 0.242
+          }
+        , { name = "(KK 206)"
+          , minsize = 0.066624
+          , speed = 0.194
+          , missdistance = 0.142
+          }
+        , { name = "(210K)"
+          , minsize = 0.076624
+          , speed = 0.394
+          , missdistance = 0.442
+          }
+        , { name = "(2015 44)"
+          , minsize = 0.031624
+          , speed = 0.594
+          , missdistance = 0.542
+          }
         ]
     , asteroidsErr = None
     }
